29a9efb2b17d262aff0cc188004f80ca7f96ea1d
Mattbalaise <106539321+Mattbalaise@users.noreply.github.com>
2026-07-29 14:18:00 +0200

    Simplify updateSession function
    Refactor updateSession to remove unused response.

--- server/supabase/middleware.ts
+++ server/supabase/middleware.ts
@@ -3,14 +3,12 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(
   request: NextRequest,
-): Promise<{ response: NextResponse; user: any }> {
-  // On crée une réponse de base
-  let supabaseResponse = NextResponse.next({ request });
+): Promise<{user: any }> {
   // On instancie le client Supabase avec accès aux cookies
   const supabase = await createClient();
   // getUser() → revalide le JWT côté Supabase
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  return { response: supabaseResponse, user };
+  return { user };
 }
